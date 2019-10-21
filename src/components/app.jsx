f8ece4d7d28759f9abbe2cd2c03370d404f72e5f
Joe Paul <joe.paul0712@gmail.com>
2019-10-21 10:51:30 +0100

    configuring components

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+import Marker from './marker';
+import flats from '../../data/flats';
+import FlatList from './flat_list';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
 
+    }
+  }
 }
 
 export default App;
