1ffe8fd072d41686dc97bb317cde4bdb939c545f
Joe Paul <joe.paul0712@gmail.com>
2019-10-21 15:47:32 +0100

    finishing flat

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -7,12 +7,12 @@ class Flat extends Component {
 
   render() {
     return (
-      <div class="card" style="background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url('https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg';);">
-        <div class="card-category">150 EUR</div>
-        <div class="card-description">
-          <h2>Super 60m2 in trendy neighborhood!</h2>
+      <div className="card" style={`card${this.props.selected ? ' active' : ''}`} style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('${this.props.flat.imageUrl}')` }}>
+        <div className="card-category">{this.props.flat.price} {this.props.flat.priceCurrency}</div>
+        <div className="card-description">
+          <h2>{this.props.flat.name}</h2>
         </div>
-        <a class="card-link" href="#"></a>
+        <a className="card-link" href="#" onClick={this.handleClick}></a>
       </div>
     );
   }
